a7028b371e7860bd23d41536201c40e562b6108b
杉浦 圭祐 <drksugi@gmail.com>
2018-06-10 00:20:41 +0900

    Code formatting only

--- src/my/test/HelloWorld.java
+++ src/my/test/HelloWorld.java
@@ -17,47 +17,47 @@ import javax.swing.JMenuItem;
 import javax.swing.JSplitPane;
 
 public class HelloWorld {
-	
-	private JFrame mMainFrame;
-	private JMenuBar mMenuBar;
-	private JMenu mMenuFile;
-	private JMenuItem mMenuItemExit;
-	private JSplitPane mSplitPane;
-	
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		
-		List<String> strList = Arrays.asList("C++", "Python", "Java", "CSharp");
-		SomeUtility.PrintListElements(strList);
-		
-		HelloWorld helloWorldApp = new HelloWorld();
-	}
-	
-	public HelloWorld() {
-		this.mMainFrame = new JFrame("EclipseGitTest");
-		this.mMainFrame.setSize(640, 480);
-		this.mMainFrame.setLocationRelativeTo(null);
-		this.mMainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.mMainFrame.setVisible(true);
-		
-		this.mMenuBar = new JMenuBar();
-		this.mMainFrame.setJMenuBar(this.mMenuBar);
-		
-		this.mMenuFile = new JMenu("File");
-		this.mMenuFile.setMnemonic(KeyEvent.VK_F);
-		this.mMenuBar.add(this.mMenuFile);
-		
-		this.mMenuItemExit = new JMenuItem("Exit");
-		this.mMenuItemExit.setMnemonic(KeyEvent.VK_X);
-		this.mMenuItemExit.addActionListener(e -> {
-			this.mMainFrame.dispatchEvent(
-				new WindowEvent(this.mMainFrame, WindowEvent.WINDOW_CLOSING));
-		});
-		this.mMenuFile.add(this.mMenuItemExit);
-		
-		this.mSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-		this.mMainFrame.getContentPane().add(this.mSplitPane, BorderLayout.CENTER);
-		
-		this.mMainFrame.pack();
-	}
+    
+    private JFrame mMainFrame;
+    private JMenuBar mMenuBar;
+    private JMenu mMenuFile;
+    private JMenuItem mMenuItemExit;
+    private JSplitPane mSplitPane;
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        List<String> strList = Arrays.asList("C++", "Python", "Java", "CSharp");
+        SomeUtility.PrintListElements(strList);
+        
+        HelloWorld helloWorldApp = new HelloWorld();
+    }
+    
+    public HelloWorld() {
+        this.mMainFrame = new JFrame("EclipseGitTest");
+        this.mMainFrame.setLocationRelativeTo(null);
+        this.mMainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.mMainFrame.setVisible(true);
+        
+        this.mMenuBar = new JMenuBar();
+        this.mMainFrame.setJMenuBar(this.mMenuBar);
+        
+        this.mMenuFile = new JMenu("File");
+        this.mMenuFile.setMnemonic(KeyEvent.VK_F);
+        this.mMenuBar.add(this.mMenuFile);
+        
+        this.mMenuItemExit = new JMenuItem("Exit");
+        this.mMenuItemExit.setMnemonic(KeyEvent.VK_X);
+        this.mMenuItemExit.addActionListener(e -> {
+            this.mMainFrame.dispatchEvent(
+                new WindowEvent(this.mMainFrame, WindowEvent.WINDOW_CLOSING));
+        });
+        this.mMenuFile.add(this.mMenuItemExit);
+        
+        this.mSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+        this.mMainFrame.getContentPane().add(this.mSplitPane, BorderLayout.CENTER);
+
+        this.mMainFrame.pack();
+        this.mMainFrame.setSize(640, 480);
+    }
 }
