66be3d196b5ece137fef6fcea6292c025f89b434
Stern Gerlach <drksugi@gmail.com>
2018-06-09 23:17:08 +0900

    Add GUI

--- src/my/test/HelloWorld.java
+++ src/my/test/HelloWorld.java
@@ -7,11 +7,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.JFrame;
+
 public class HelloWorld {
+	
+	private JFrame mMainFrame;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		
 		List<String> strList = Arrays.asList("C++", "Python", "Java", "CSharp");
 		SomeUtility.PrintListElements(strList);
+		
+		HelloWorld helloWorldApp = new HelloWorld();
+	}
+	
+	public HelloWorld() {
+		this.mMainFrame = new JFrame("EclipseGitTest");
+		this.mMainFrame.setSize(640, 480);
+		this.mMainFrame.setLocationRelativeTo(null);
+		this.mMainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.mMainFrame.setVisible(true);
 	}
 }
