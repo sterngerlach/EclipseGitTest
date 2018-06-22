6716c2eafc6b6bd14e1546a90ed1b9b20a20da61
ua213579 <ua213579@y12a027.educ.cc.keio.ac.jp>
2018-06-22 12:37:58 +0900

    fizzbuzzをかえました

--- src/my/test/SomeUtility.java
+++ src/my/test/SomeUtility.java
@@ -21,8 +21,8 @@ public class SomeUtility {
     }
     
     public static void fizzbuzz(int n) {
-        if (n % 3 == 0) { System.out.println("fizz"); }
-        else if(n % 5 == 0) { System.out.println("buzz"); }
+        if (n % 3 == 0) { System.out.println("ffffffiiiiizzzzzzz"); }
+        else if(n % 5 == 0) { System.out.println("bbbbbbuuuuuuzzzzzz"); }
         else { System.out.println(n);}
     }
     
